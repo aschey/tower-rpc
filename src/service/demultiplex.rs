09d41c147c3e69cf79d8b0d490459d03e0f345b6
Austin Schey <aschey13@gmail.com>
2023-05-01 18:59:25 -0500

    split services

---
+++ src/service/demultiplex.rs
@@ -0,0 +1,39 @@
+use futures::Future;
+use std::{marker::PhantomData, pin::Pin, task::Poll};
+
+#[derive(Clone, Debug)]
+pub struct DemultiplexService<S, Res> {
+    inner: S,
+    _phantom: PhantomData<Res>,
+}
+
+impl<S, Res> DemultiplexService<S, Res> {
+    pub fn new(inner: S) -> Self {
+        Self {
+            inner,
+            _phantom: Default::default(),
+        }
+    }
+}
+
+impl<S, Req, Res> tower::Service<Req> for DemultiplexService<S, Res>
+where
+    Req: Send,
+    S: tower::Service<crate::Tagged<Req>, Response = crate::Tagged<Res>>,
+    S::Future: Send + 'static,
+{
+    type Error = S::Error;
+    type Response = Res;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    fn poll_ready(&mut self, cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Req) -> Self::Future {
+        let res = self.inner.call(crate::Tagged { tag: 0, value: req });
+        Box::pin(async move {
+            let res = res.await?;
+            Ok(res.value)
+        })
+    }
+}
